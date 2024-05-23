3a55eaaf5c62561ba1241408a2e975a194a0371d
AshutoshVohra <ashutoshvohra136@gmail.com>
2024-05-24 04:10:50 +0530

    Implemented Crud Functionality for user Post

--- app.js
+++ app.js
@@ -23,4 +23,43 @@ app.use(checkForAuthenticationCookie("token"));
 
 app.get("/", (req, res) => res.json({ user: req.user }));
 
+app.get("/userpost", async (req, res) => {
+  const allPost = await UserPost.find({});
+
+  return res.status(200).json({ "All-Post": allPost });
+});
+
+app.post("/userpost", async (req, res) => {
+  const { content, createdBy } = req.body;
+  const userpost = await UserPost.create({ content, createdBy });
+  return res.status(201).json({ message: "Post Created", userPost: userpost });
+});
+
+app.put("/userpost/update/:id", async (req, res) => {
+  try {
+    let userPost = await UserPost.findById(req.params.id);
+    if (!userPost) return res.status(404).json({ message: "Post Not Found" });
+
+    userpost = await UserPost.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    return res.status(200).json({ userpost: userpost });
+  } catch (error) {
+    return res.status(500).json({ error: "Server Error" });
+  }
+});
+
+app.delete("/userpost/delete/:id", async (req, res) => {
+  try {
+    let userpost = await UserPost.findById(req.params.id);
+    if (!userpost) return res.status(404).json({ message: "Post Not Found" });
+
+    const userPost = await UserPost.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ message: "Post Deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+});
+
 app.listen(PORT, () => console.log(`Server running on PORT : ${PORT}`));
